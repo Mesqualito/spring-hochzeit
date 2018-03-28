4afd29cbf695a60f69bdd1525860a95d36516716
Jochen Haßfurter <jo@hassfurter.com>
2018-03-28 18:31:59 +0200

    First local commit
    Signed-off-by: Jochen Haßfurter <jo@hassfurter.com>

---
+++ src/main/groovy/rocks/gebsattel/hochzeit/HochzeitApplication.groovy
@@ -0,0 +1,12 @@
+package rocks.gebsattel.hochzeit
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HochzeitApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run HochzeitApplication, args
+	}
+}
