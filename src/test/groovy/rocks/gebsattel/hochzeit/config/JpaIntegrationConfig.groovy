380872851b57e580b545971d472735312406ebb7
Jochen Haßfurter <jo@hassfurter.com>
2018-04-13 10:52:57 +0200

    Testing JPA-Integration (without Apache Tomcat booting up)
    Signed-off-by: Jochen Haßfurter <jo@hassfurter.com>

---
+++ src/test/groovy/rocks/gebsattel/hochzeit/config/JpaIntegrationConfig.groovy
@@ -0,0 +1,13 @@
+package rocks.gebsattel.hochzeit.config
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("rocks.gebsattel.hochzeit")
+class JpaIntegrationConfig {
+
+
+}
---
+++ src/test/groovy/rocks/gebsattel/hochzeit/services/ProductServiceJapDaoImplTest.groovy
@@ -0,0 +1,29 @@
+package rocks.gebsattel.hochzeit.services
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import rocks.gebsattel.hochzeit.domain.Product
+
+@RunWith(SpringJUnit4ClassRunner)
+@SpringBootTest("JpaIntegrationConfig")
+@ActiveProfiles("jpadao") // keep test independent from application's "application properties"!
+class ProductServiceJapDaoImplTest {
+
+    private ProductService productService
+
+    @Autowired
+    void setProductService(ProductService productService) {
+        this.productService = productService
+    }
+
+    @Test
+    void testListMethod() throws Exception {
+
+        List <Product> products = (List<Product>) productService.listAll()
+        assert products.size() == 5
+    }
+}
