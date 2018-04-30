10c552da4f1d322bbd868ad8981d617699cc3ff7
Jochen Hassfurter <jo@hassfurter.com>
2018-05-01 01:41:59 +0200

    Revert "Initial local macbook commit for branch "jhipster_weddingApp""
    This reverts commit b0daec9b519409ab3cae293bde753a028157d5c0.
    
    # Conflicts:
    #	.idea/modules/wedding-app.iml

---
+++ src/main/java/rocks/gebsattel/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package rocks.gebsattel.config.audit;
---
+++ src/main/java/rocks/gebsattel/repository/search/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data Elasticsearch repositories.
+ */
+package rocks.gebsattel.repository.search;
